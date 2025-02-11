80f7f6b0755ecacbaea47b1d6593bdc74cf38611
Cacapon Project <cacapon.project@gmail.com>
2025-02-11 21:14:50 +0900

    必要最低限の機能作成

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 19:07:46 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/02/11 21:03:28 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/02/11 21:12:30 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,24 @@ void	exec_cmd1(int pipefd[2], char *file1, char *cmd1, char **envp)
 	exit(1);
 }
 
+void	exec_cmd2(int pipefd[2], char *cmd2, char *file2, char **envp)
+{
+	int		fd;
+	char	**args;
+	char	*bin_path;
+
+	fd = open(file2, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd < 0)
+		exit(1);
+	dup2(pipefd[0], STDIN_FILENO);
+	dup2(fd, STDOUT_FILENO);
+	close(pipefd[1]);
+	args = ft_split(cmd2, ' ');
+	bin_path = get_command_path(args[0], envp);
+	execve(bin_path, args, envp);
+	exit(1);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	char	*line;
@@ -61,16 +79,11 @@ int	main(int argc, char **argv, char **envp)
 	if (argc != 5)
 		return (1);
 	pipe(pipefd);
-	ft_printf("DEBUG: %s %s %s %s\n", argv[1], argv[2], argv[3], argv[4]);
 	if (fork() == 0)
 		exec_cmd1(pipefd, argv[1], argv[2], envp);
+	if (fork() == 0)
+		exec_cmd2(pipefd, argv[3], argv[4], envp);
 	close(pipefd[1]);
-	line = get_next_line(pipefd[0]);
-	while (line != NULL)
-	{
-		ft_printf("%s", line);
-		free(line);
-		line = get_next_line(pipefd[0]);
-	}
+	close(pipefd[0]);
 	return (0);
 }
