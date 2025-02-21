3f12398ce049c448fc370cf98094a85cd96452e7
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-02-21 15:46:00 +0900

    fdを外部に切り出し

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 19:07:46 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/02/21 15:38:41 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/02/21 15:44:02 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,25 +58,15 @@ void	exec_common(int close_fd, char *cmd, char **envp)
 	exit(1);
 }
 
-void	exec_cmd1(int pipefd[2], char *file1, char *cmd1, char **envp)
+void	exec_cmd1(int fd, int pipefd[2], char *cmd1, char **envp)
 {
-	int		fd;
-
-	fd = open(file1, O_RDONLY);
-	if (fd < 0)
-		exit(1);
 	dup2(fd, STDIN_FILENO);
 	dup2(pipefd[1], STDOUT_FILENO);
 	exec_common(pipefd[0], cmd1, envp);
 }
 
-void	exec_cmd2(int pipefd[2], char *cmd2, char *file2, char **envp)
+void	exec_cmd2(int fd, int pipefd[2], char *cmd2, char **envp)
 {
-	int		fd;
-
-	fd = open(file2, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd < 0)
-		exit(1);
 	dup2(pipefd[0], STDIN_FILENO);
 	dup2(fd, STDOUT_FILENO);
 	exec_common(pipefd[1], cmd2 ,envp);
@@ -85,14 +75,19 @@ void	exec_cmd2(int pipefd[2], char *cmd2, char *file2, char **envp)
 int	main(int argc, char **argv, char **envp)
 {
 	int	pipefd[2];
+	int i_o[2];
 
 	if (argc != 5)
 		return (1);
+	i_o[0] = open(argv[1], O_RDONLY);
+	i_o[1] = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (i_o[0] < 0 || i_o[1] < 0)
+		return (1);
 	pipe(pipefd);
 	if (fork() == 0)
-		exec_cmd1(pipefd, argv[1], argv[2], envp);
+		exec_cmd1(i_o[0], pipefd, argv[2], envp);
 	if (fork() == 0)
-		exec_cmd2(pipefd, argv[3], argv[4], envp);
+		exec_cmd2(i_o[1], pipefd, argv[3], envp);
 	close(pipefd[1]);
 	close(pipefd[0]);
 	return (0);
